1d3a7bf8eaa8e75adaa80d96513a6da3e3e7a1f3
YoussefEmad <youssef1emad@gmail.com>
2018-08-16 23:15:33 +0200

    updated

--- app/src/main/java/com/youssef1emad/google_map/MapsActivity.java
+++ app/src/main/java/com/youssef1emad/google_map/MapsActivity.java
@@ -1,23 +1,32 @@
 package com.youssef1emad.google_map;
 
 import android.Manifest;
+
 import android.annotation.SuppressLint;
+import android.content.Context;
 import android.content.pm.PackageManager;
 import android.location.Address;
 import android.location.Geocoder;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.support.v4.app.FragmentTransaction;
+import android.view.Gravity;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
@@ -27,9 +36,14 @@ import com.google.android.gms.maps.model.MarkerOptions;
 import java.io.IOException;
 import java.util.List;
 
+import static android.app.PendingIntent.getActivity;
+import static java.security.AccessController.getContext;
+
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
+    EditText editText2;
+    Button btn;
 
     /**youssef Emad android developer**/
     @Override
@@ -39,20 +53,32 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+
+
     }
 
 
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
+
+
+
+
+
+
+
+
+
+
+        /**
+         * Manipulates the map once available.
+         * This callback is triggered when the map is ready to be used.
+         * This is where we can add markers or lines, add listeners or move the camera. In this case,
+         * we just add a marker near Sydney, Australia.
+         * If Google Play services is not installed on the device, the user will be prompted to install
+         * it inside the SupportMapFragment. This method will only be triggered once the user has
+         * installed Google Play services and returned to the app.
+         */
     @Override
-    public void onMapReady(GoogleMap googleMap) {
+    public void onMapReady( GoogleMap googleMap) {
         mMap = googleMap;
 
         if (mMap != null) {
@@ -64,58 +90,61 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
                 @Override
                 public View getInfoContents(Marker marker) {
-                    View v = getLayoutInflater().inflate(R.layout.info_window, null);
-                    TextView tvLocality = v.findViewById(R.id.tv_locality);
-                    TextView tvLat = v.findViewById(R.id.tv_lat);
-                    TextView tvLog = v.findViewById(R.id.tv_lng);
-                    TextView tvSnippet = v.findViewById(R.id.tv_snippet);
+                    View v = getLayoutInflater().inflate(R.layout.clickable_form, null);
+                   // TextView tvLocality = v.findViewById(R.id.tv_locality);
+                    //TextView tvLat = v.findViewById(R.id.tv_lat);
+
+                    //TextView tvLog = v.findViewById(R.id.tv_lng);
+                    //TextView tvSnippet = v.findViewById(R.id.tv_snippet);
+                    EditText etc = (EditText)v.findViewById(R.id.editText2);
+                    Button btn = (Button)v.findViewById(R.id.btn);
+
 
                     LatLng ll = marker.getPosition();
-                    tvLocality.setText(marker.getTitle());
-                    tvLat.setText("latitude" + ll.latitude);
-                    tvLog.setText("longitude" + ll.longitude);
-                    tvSnippet.setText(marker.getSnippet());
+                    //tvLocality.setText(marker.getTitle());
+                    //tvLat.setText("latitude" + ll.latitude);
+                    //tvLog.setText("longitude" + ll.longitude);
+                    //tvSnippet.setText(marker.getSnippet());
 
                     return v;
                 }
             });
+
         }
 
-        // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
-        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            // TODO: Consider calling
-            //    ActivityCompat#requestPermissions
-            // here to request the missing permissions, and then overriding
-            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-            //                                          int[] grantResults)
-            // to handle the case where the user grants the permission. See the documentation
-            // for ActivityCompat#requestPermissions for more details.
-            return;
-        }
+
         mMap.setMyLocationEnabled(true);
     }
+
+
+        // Return false to indicate that we have not consumed the event and that we wish
+        // for the default behavior to occur (which is for the camera to move such that the
+        // marker is centered and for the marker's info window to open, if it has one).
+
+
+
 
     private void goToLocation (double lat , double lng) {
         LatLng ll = new LatLng(lat, lng);
         CameraUpdate update = CameraUpdateFactory.newLatLng(ll);
         mMap.moveCamera(update);
     }
+
     private void goToLocationZoom(Double lat, Double lng, float zoom)
     {
         LatLng li = new LatLng(lat,lng);
         CameraUpdate update = CameraUpdateFactory.newLatLngZoom(li, zoom);
         mMap.moveCamera(update);
+
     }
-    Marker marker;
+
+    //Marker marker;
     public void geoLocate(View view)  throws IOException
     {Marker marker;
         EditText stopPort = findViewById(R.id.editText);
         if (stopPort.getText().toString().length() > 0)
         {
-            String location = stopPort.getText().toString().trim();
+            String location = stopPort.getText().toString();
             Geocoder gc = new Geocoder(this);
             List<Address> list = gc.getFromLocationName(location, 1);
             if (list != null && list.size() != 0)
@@ -172,5 +201,20 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    public void save (View view){
+        Marker marker;
+        Button button = (Button)findViewById(R.id.btn);
+        EditText editText = (EditText)findViewById(R.id.editText2);
+        Toast.makeText(MapsActivity.this, "fff", Toast.LENGTH_SHORT).show();
+
+        mMap.setOnMapClickListener(new GoogleMap.OnMapClickListener() {
+
+            @Override
+            public void onMapClick(LatLng latLng) {
+                Toast.makeText(MapsActivity.this, "fff", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
